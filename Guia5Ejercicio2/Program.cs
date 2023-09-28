c4b3d78267dc963c9e132389fb8055f2b440b2ca
sujey22 <da0089032022@unab.edu.sv>
2023-09-28 14:37:35 -0600

    Create Program.cs

---
+++ Guia5Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+//Ejercio 2 con funciones de fecha
+class Program
+{
+    public static void Main()
+    {
+        double montoPrestamo = 3000.00;
+        int plazoMeses = 6;
+        DateTime fechaInicio = DateTime.Now;
+
+        
+        double cuotaMensual = montoPrestamo / plazoMeses;
+
+        Console.WriteLine("Detalles del préstamo:");
+        Console.WriteLine("Estudiante: Juan Perez");
+        Console.WriteLine("Lugar donde estudia: UNAB Chalatenango");
+        Console.WriteLine($"Monto del préstamo: ${montoPrestamo}");
+        Console.WriteLine($"Plazo: {plazoMeses} meses");
+
+        Console.WriteLine("\nFechas para realizar el pago y sus cuotas mensuales:");
+        for (int i = 0; i < plazoMeses; i++)
+        {
+            
+            DateTime FechaPago = fechaInicio.AddMonths(i);
+
+            
+            Console.WriteLine($"{FechaPago.ToString("dd MMMM yyyy")}: Cuota de ${cuotaMensual}");
+        }
+    }
+}
